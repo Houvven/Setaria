5b48f85e8857031c089ce36a44078350e799a777
Houvven <houvven@gmail.com>
2025-01-24 22:06:31 +0800

    feat(hook): add hook for launcher
    - Adds a hook to hide the recent-view clear button in the launcher.

---
+++ hook/src/main/java/com/houvven/setaria/hook/utils/RClassSigned.java
@@ -0,0 +1,9 @@
+package com.houvven.setaria.hook.utils;
+
+public class RClassSigned {
+    private RClassSigned() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    public static final String ID = "R$id";
+}
