f5de858a11eb3c31fe5aa2e7a9a5a8c481bc821e
houvven <houvven@gmail.com>
2025-04-09 23:13:45 +0800

    chore(build): update compileSdk and targetSdk to version 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.houvven.setaria"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.houvven.setaria"
         minSdk = 31
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
